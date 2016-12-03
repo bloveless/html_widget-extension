d81f8f5ca7770f9529700cfcaa6dcde6199dd663
Brennon Loveless <brennon.loveless@gmail.com>
2016-12-03 15:41:23 -0700

    Remove routes since they are not needed.

--- resources/routes/html_widget.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'admin/html_widget/html_widget' => 'Fritzandandre\HtmlWidgetExtension\Http\Controller\Admin\HtmlWidgetController@index',
-    'admin/html_widget/html_widget/create' => 'Fritzandandre\HtmlWidgetExtension\Http\Controller\Admin\HtmlWidgetController@create',
-    'admin/html_widget/html_widget/edit/{id}' => 'Fritzandandre\HtmlWidgetExtension\Http\Controller\Admin\HtmlWidgetController@edit'
-];
