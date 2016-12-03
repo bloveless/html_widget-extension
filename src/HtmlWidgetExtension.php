d6b568daaecbf4427f3a0257d40b0c2f7f9c6134
Brennon Loveless <brennon.loveless@gmail.com>
2016-12-03 15:27:05 -0700

    Add display support.

--- src/HtmlWidgetExtension.php
+++ src/HtmlWidgetExtension.php
@@ -2,6 +2,7 @@
 
 use Anomaly\Streams\Platform\Addon\Extension\Extension;
 use Fritzandandre\HtmlWidgetExtension\HtmlWidget\Form\HtmlWidgetFormBuilder;
+use Fritzandandre\HtmlWidgetExtension\HtmlWidget\HtmlWidgetModel;
 use Fritzandandre\LayoutFieldType\Contract\LayoutExtensionInterface;
 
 class HtmlWidgetExtension extends Extension implements LayoutExtensionInterface
@@ -14,6 +15,29 @@ class HtmlWidgetExtension extends Extension implements LayoutExtensionInterface
     protected $provides = 'fritzandandre.field_type.layout::widget.html';
 
     /**
+     * The model for this widget.
+     *
+     * @var HtmlWidgetModel
+     */
+    protected $model;
+
+    /**
+     * Then entry ID to use when retrieving the layout.
+     * @var
+     */
+    protected $entryId;
+
+    /**
+     * HtmlWidgetExtension constructor.
+     *
+     * @param HtmlWidgetModel $model
+     */
+    public function __construct(HtmlWidgetModel $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
      * Get the form used to create and edit html widgets.
      *
      * @return \Illuminate\Foundation\Application|mixed
@@ -21,5 +45,26 @@ class HtmlWidgetExtension extends Extension implements LayoutExtensionInterface
     public function getForm()
     {
         return app(HtmlWidgetFormBuilder::class);
+    }
+
+    /**
+     * Set the entry id for rendering.
+     *
+     * @param $entryId
+     */
+    public function setEntryId($entryId)
+    {
+        $this->entryId = $entryId;
+    }
+
+    /**
+     * Render the content.
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function render()
+    {
+        $htmlWidget = $this->model->find($this->entryId);
+        return view('fritzandandre.extension.html_widget::render', ['content' => $htmlWidget->html]);
     }
 }
